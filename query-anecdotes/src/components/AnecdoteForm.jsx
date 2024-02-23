ac06fa50778d17b7050b6913c0ae01df1cae4110
Gary Crosby <21145626+GeraBogdanov@users.noreply.github.com>
2024-02-23 18:31:27 +0300

    Add anecdote post using React Query

--- query-anecdotes/src/components/AnecdoteForm.jsx
+++ query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,9 +1,22 @@
+import { QueryClient, useMutation, useQueryClient } from "@tanstack/react-query"
+import { createAnecdote } from "../requests"
+
 const AnecdoteForm = () => {
+  const QueryClient = useQueryClient()
+
+  const newAnecdoteMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: (newAnecdote) => {
+      const anecdotes = QueryClient.getQueryData(['anecdotes'])
+      QueryClient.setQueriesData(['anecdotes'], anecdotes.concat(newAnecdote))
+    }
+  })
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
+    newAnecdoteMutation.mutate({ content, votes: 0})
     console.log('new anecdote')
 }
 
